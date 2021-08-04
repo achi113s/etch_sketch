0932df62bc28258c6d2e99f150b926ff4a5ba0dc
glat1957 <giorgio2496@gmail.com>
2021-08-03 22:02:25 -0600

    got grid set up, can click each cell to change color

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,24 @@
-const frame = document.querySelector("div#frame");
+const canvas = document.querySelector("div.canvas");
+const cell_size = "40px";
 
-frame.setAttribute('style', 'display: inline-flex; flex-wrap: wrap;');
+canvas.setAttribute('style', `display: grid; grid-template-columns: repeat(16, ${cell_size})`);
 
 // first use a loop to create 16 divs and then add them to the frame div
-for (let i = 0; i <= 16; i++) {
-    for (let j = 0; j <= 16; j++) {
+for (let i = 1; i <= 16; i++) {
+    for (let j = 1; j <= 16; j++) {
         const newdiv = document.createElement('div');
-        newdiv.setAttribute('id', `${i}_${j}`);
-        newdiv.setAttribute('style', 'border: 1px solid red; height: 10px; width: 10px');
-        frame.appendChild(newdiv);
+        newdiv.setAttribute('class', `cell ${i}_${j}`);
+        newdiv.setAttribute('style', `border: 0.5px solid gray; height: ${cell_size}; width: ${cell_size}`);
+        canvas.appendChild(newdiv);
         console.log(`appended div ${i}_${j}`)
-    }
-    
+    } 
 }
+
+const cells = document.querySelectorAll("div.cell");
+
+cells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+        cell.setAttribute('style', 'background-color: black; border: 0.5px solid black;');
+    });
+});
+
