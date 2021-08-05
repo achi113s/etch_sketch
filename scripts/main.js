bb3400143932d867e2469b61319098de2767b073
glat1957 <giorgio2496@gmail.com>
2021-08-05 10:53:44 -0600

    can draw now, borders are very slightly off, centered canvas

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,25 @@
 const canvas = document.querySelector("div.canvas");
+canvas.setAttribute('style', 'margin: auto; width: 100%;')
 const cell_size = "40px";
 
-canvas.setAttribute('style', `display: grid; grid-template-columns: repeat(16, ${cell_size})`);
+canvas.setAttribute('style', `display: grid; grid-template-columns: repeat(16, ${cell_size}); justify-content: center;`);
 
 // first use a loop to create 16 divs and then add them to the frame div
 for (let i = 1; i <= 16; i++) {
     for (let j = 1; j <= 16; j++) {
         const newdiv = document.createElement('div');
-        newdiv.setAttribute('class', `cell ${i}_${j}`);
-        newdiv.setAttribute('style', `border: 0.5px solid gray; height: ${cell_size}; width: ${cell_size}`);
+        newdiv.classList.add(`cell`);
+        newdiv.setAttribute('style', `border: 1px solid lightgray; height: ${cell_size}; width: ${cell_size};`);
         canvas.appendChild(newdiv);
-        console.log(`appended div ${i}_${j}`)
+        // console.log(`appended div ${i}_${j}`)
     } 
 }
 
 const cells = document.querySelectorAll("div.cell");
 
 cells.forEach((cell) => {
-    cell.addEventListener('click', () => {
-        cell.setAttribute('style', 'background-color: black; border: 0.5px solid black;');
+    cell.addEventListener('mouseover', () => {
+        cell.classList.add('drawn');
     });
 });
 
