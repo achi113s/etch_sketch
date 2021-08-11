16a914dc1696b1af2f02a4a1bcded89983acd975
glat1957 <giorgio2496@gmail.com>
2021-08-10 20:06:21 -0600

    starting to make functions to do repetitive tasks like redraw the canvas

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,25 @@
-const canvas = document.querySelector("div.canvas");
-canvas.setAttribute('style', 'margin: auto; width: 100%;')
-
 const canvas_width = 640;
+
+function drawCanvas(canvas_divisions, cell_size, canvas) {
+    canvas.setAttribute('style', `display: grid; grid-template-columns: repeat(${canvas_divisions}, ${cell_size}px); justify-content: center;`);
+    // first use a loop to create x divs and then add them to the frame div
+    for (let i = 1; i <= canvas_divisions; i++) {
+        for (let j = 1; j <= canvas_divisions; j++) {
+            const newdiv = document.createElement('div');
+            newdiv.classList.add(`cell`);
+            newdiv.setAttribute('style', `border: 1px solid gray; height: ${cell_size}px; width: ${cell_size}px;`)
+            canvas.appendChild(newdiv);
+        }
+    }
+}
+
+function removeCanvas(canvas) {
+    canvas.remove();
+}
+
 let canvas_divisions = 16;
 let cell_size = canvas_width / canvas_divisions;
-
-canvas.setAttribute('style', `display: grid; grid-template-columns: repeat(${canvas_divisions}, ${cell_size}px); justify-content: center;`);
-
-// first use a loop to create x divs and then add them to the frame div
-for (let i = 1; i <= canvas_divisions; i++) {
-    for (let j = 1; j <= canvas_divisions; j++) {
-        const newdiv = document.createElement('div');
-        newdiv.classList.add(`cell`);
-        newdiv.setAttribute('style', `border: 1px solid gray; height: ${cell_size}px; width: ${cell_size}px;`)
-        canvas.appendChild(newdiv);
-    } 
-}
+drawCanvas(canvas_divisions, cell_size, canvas)
 
 // drawing capability, mouseover event colors each cell
 const cells = document.querySelectorAll("div.cell");
@@ -37,7 +41,9 @@ document.querySelector('body').appendChild(buttons_div);
 // event listener for clear button, removes the "drawn" class from each cell
 clear_btn.addEventListener('click', () => {
     cells.forEach((cell) => {
-        cell.classList.remove('drawn');
+        // cell.classList.remove('drawn');
+        removeCanvas(canvas);
+        drawCanvas(canvas_divisions, cell_size, canvas);
     });
 });
 
