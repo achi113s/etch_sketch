79abc99c0c52db4648109fbcd328f9b05faec320
glat1957 <giorgio2496@gmail.com>
2021-08-16 21:06:52 -0600

    everything works now and it's fairly fast, code kinda ugly

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,16 @@
 const canvas_side = 640;
+const default_color = '#333'
 let color_mode = 'default';
 
 function draw_cells(canvas_divisions) {
     // set up the CSS grid on the canvas div
     let canvas_div = document.querySelector('div#canvas');
-    canvas_div.setAttribute('style', `display: grid; grid-template-columns: repeat(${canvas_divisions}, 1fr); grid-template-rows: repeat(${canvas_divisions}, 1fr); box-shadow: 0px 0px 10px gray; height: ${canvas_side}px; width: ${canvas_side}px;`);
+    canvas_div.setAttribute('style', `display: grid; grid-template-columns: repeat(${canvas_divisions}, 1fr); grid-template-rows: repeat(${canvas_divisions}, 1fr);`);
     // now use a loop to create the cells and then add them to the canvas div
-    for (let i = 1; i <= canvas_divisions; i++) {
-        for (let j = 1; j <= canvas_divisions; j++) {
-            const newdiv = document.createElement('div');
-            newdiv.classList.add(`cell`);
-            canvas_div.appendChild(newdiv);
-        }
+    for (let i = 1; i <= canvas_divisions*canvas_divisions; i++) {
+        const newdiv = document.createElement('div');
+        newdiv.classList.add(`cell`);
+        canvas_div.appendChild(newdiv);
     }
 
     // drawing capability, mouseover event colors each cell
@@ -19,12 +18,14 @@ function draw_cells(canvas_divisions) {
     cells.forEach((cell) => {
         cell.addEventListener('mouseover', () => {
             if (color_mode == 'rainbow') {
-                let color = Math.floor(Math.random()*16777215).toString(16);
-                cell.setAttribute('style', `background-color: #${color};`);
+                let r = Math.floor(Math.random()*256);
+                let g = Math.floor(Math.random()*256);
+                let b = Math.floor(Math.random()*256);
+                cell.setAttribute('style', `background-color: rgb(${r}, ${g}, ${b});`);
                 // cell.classList.add('drawn_rainbow');
             } else {
                 // cell.classList.add('drawn');
-                cell.setAttribute('style', `background-color: black;`);
+                cell.setAttribute('style', `background-color: ${default_color};`);
 
             }
             
@@ -59,16 +60,16 @@ clear_btn.addEventListener('click', () => {
     redraw_cells();
 });
 
+const current_grid_res = document.querySelector('div#current_grid_res');
+current_grid_res.setAttribute('style', 'text-align: center;');
 const grid_res_slider = document.querySelector('input#grid_res_slider');
 // event listener for the grid resolution slider
 grid_res_slider.addEventListener('click', () => {
-    current_grid_res.textContent = `${grid_res_slider.value}`;
+    current_grid_res.textContent = `${grid_res_slider.value} x ${grid_res_slider.value}`;
     redraw_cells();
 });
 
-const current_grid_res = document.querySelector('div#current_grid_res');
-current_grid_res.setAttribute('style', 'text-align: center;');
-current_grid_res.textContent = `${grid_res_slider.value}`;
+current_grid_res.textContent = `${grid_res_slider.value} x ${grid_res_slider.value}`;
 
 let canvas_divisions = 16;
 draw_cells(canvas_divisions);
